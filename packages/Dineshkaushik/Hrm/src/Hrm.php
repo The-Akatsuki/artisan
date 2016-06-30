565bf5bc4656e573ac712c5e7caee82620e095d0
Dinesh Kumar Sharma <dinesh829269@gmail.com>
2016-06-30 08:08:12 +0530

    HRM SETUP and watchtower installed

---
+++ packages/Dineshkaushik/Hrm/src/Hrm.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace Dineshkaushik\Hrm;
+use App\Http\Controllers\Controller;
+
+
+class Hrm extends Controller{
+ 
+    
+    public function __construct()
+    {
+      
+    }
+    
+	// You code here.
+    public function test(){
+        return "HI";
+    }
+
+}
