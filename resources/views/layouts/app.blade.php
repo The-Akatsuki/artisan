0943ccda6bdd3776facbe96d34d444f8b155d0ca
Dinesh Kumar Sharma <dinesh829269@gmail.com>
2016-07-09 17:48:27 +0530

    Top Men Bar Changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,89 +1,101 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+        <title>Laravel</title>
 
-    <!-- Fonts -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
 
-    <!-- Styles -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+        <!-- Styles -->
+        <!-- Material Design fonts -->
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700">
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/icon?family=Material+Icons">
 
-    <style>
-        body {
-            font-family: 'Lato';
-        }
+        <!-- Bootstrap -->
+        <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 
-        .fa-btn {
-            margin-right: 6px;
-        }
-    </style>
-</head>
-<body id="app-layout">
-    <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
+        <!-- Bootstrap Material Design -->
+        <link rel="stylesheet" type="text/css" href="{{ url('public/dist/css/bootstrap-material-design.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ url('public/dist/css/ripples.min.css') }}">
+        {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
 
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+        <style>
+            body {
+                font-family: 'Lato';
+            }
 
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    HRM
-                </a>
-            </div>
+            .fa-btn {
+                margin-right: 6px;
+            }
+        </style>
+    </head>
+    <body id="app-layout">
+        <nav class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
 
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Home</a></li>
-			<li class="dropdown">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Company <span class="caret"></span></a>
-				<ul class="dropdown-menu" role="menu">
-					<li><a href="{{ url('/company_information') }}">Company</a></li>
-					<li><a href="{{ url('/department') }}">Departments</a></li>
-					<li><a href="{{ url('/watchtower') }}">Manage Roles and Permissions</a></li>				 
-				</ul>
-			</li>
+                    <!-- Collapsed Hamburger -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                    <!-- Branding Image -->
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        HRM
+                    </a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav">
+                        <li><a href="{{ url('/home') }}">Home</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Company <span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/company_information') }}">Company</a></li>
+                                <li><a href="{{ url('/department') }}">Departments</a></li>
+                                <li><a href="{{ url('/watchtower') }}">Manage Roles and Permissions</a></li>				 
+                            </ul>
+                        </li>
 
 
-			<li class="dropdown">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Employee <span class="caret"></span></a>
-				<ul class="dropdown-menu" role="menu">
-					<li><a href="{{ url('/employee') }}"></i>Employee</a></li>
-					<li><a href="{{ url('/job_type') }}">Employee/Job Type</a></li>
-					<li><a href="{{ url('/job_title') }}">Job title</a></li>
-					<li><a href="{{ url('/pay_grade') }}"></i>Pay Grade</a></li>
-                                        <li><a href="{{ url('/skills') }}"></i>Skills</a></li>
-                                        <li><a href="{{ url('/qualifications') }}"></i>Qualifications</a></li>
-                                        <li><a href="{{ url('/certifications') }}"></i>Certifications</a></li>
-                                        <li><a href="{{ url('/employee_skills') }}"></i>employee_skills</a></li>
-                                        <li><a href="{{ url('/employee_education') }}"></i>employee_education</a></li>
-                                        <li><a href="{{ url('/employee_certifications') }}"></i>employee_certifications</a></li>
-                                        <li><a href="{{ url('/employee_documents') }}"></i>employee_documents</a></li>
-                                </ul>
-			</li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Employee <span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/employee') }}"></i>Employee</a></li>
+                                <li><a href="{{ url('/employee_skills') }}"></i>employee_skills</a></li>
+                                <li><a href="{{ url('/employee_education') }}"></i>employee_education</a></li>
+                                <li><a href="{{ url('/employee_certifications') }}"></i>employee_certifications</a></li>
+                                <li><a href="{{ url('/employee_documents') }}"></i>employee_documents</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Configuration <span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/job_type') }}">Job Type</a></li>
+                                <li><a href="{{ url('/job_title') }}">Job title</a></li>
+                                <li><a href="{{ url('/pay_grade') }}"></i>Pay Grade</a></li>
+                                <li><a href="{{ url('/skills') }}"></i>Skills</a></li>
+                                <li><a href="{{ url('/qualifications') }}"></i>Qualifications</a></li>
+                                <li><a href="{{ url('/certifications') }}"></i>Certifications</a></li>                                
+                            </ul>
+                        </li>
 
-                </ul>
+                    </ul>
 
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
+                    <!-- Right Side Of Navbar -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <!-- Authentication Links -->
+                        @if (Auth::guest())
                         <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
-                    @else
+                        @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                 {{ Auth::user()->name }} <span class="caret"></span>
@@ -93,22 +105,22 @@
                                 <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                             </ul>
                         </li>
-                    @endif
-                </ul>
+                        @endif
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
 
-    @yield('content')
+        @yield('content')
 
-    <!-- JavaScripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
-<script>
-    $(document).ready(function(){
-        init_page();
-    });
-</script>
+        <!-- JavaScripts -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+        {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+    <script>
+$(document).ready(function () {
+    init_page();
+});
+    </script>
 </body>
 </html>
